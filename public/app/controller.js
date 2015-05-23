71237b2b11a4aadabf19b1f2a6a7d1c0c22967fa
Nashville Software School <nss@Nashvilles-MacBook-Pro-3.local>
2015-05-23 14:33:55 -0500

    my new need

--- public/app/controller.js
+++ public/app/controller.js
@@ -2,4 +2,19 @@ var controllers = angular.module('myapp.controllers', []);
 
 controllers.controller('IndexController', ['$scope', function($scope){
   $scope.message = 'Hello I am the HOME PAGE';
-}])
+}]);
+
+controllers.controller('AboutController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the ABOUT PAGE';
+}]);
+
+controllers.controller('NeedsController', ['$scope', function($scope){
+  $scope.message = 'Hello I am the NEEDS PAGE';
+  $scope.needs=[];
+
+  $scope.addNeeds=function(thingy){
+    $scope.needs.push(thingy);
+    $scope.newNeed="";
+  };
+
+}]);
--- public/app/controllers.js
+++
@@ -1,5 +0,0 @@
-var controllers = angular.module('myapp.controllers', []);
-
-controllers.controller('IndexController', ['$scope', function($scope){
-  $scope.message = 'Hello I am the HOME PAGE';
-}])
