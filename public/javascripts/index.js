26a2230bab0381a66eeacdba1925f9e498097621
Zoe Ames <zoeames@gmail.com>
2015-05-23 09:19:58 -0500

    initial files 2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,8 @@
 (function(){
   'use strict';
 
-  angular.module('demo', [])
+  angular.module('myapp', [])
   .controller('MainController', ['$scope', function($scope){
     $scope.name="Zoe";
-    $scope.instructors = [
-      {name:'steve', allergies:'sunshine'},
-      {name:'joe', allergies:'everything'},
-      {name:'zoe', allergies:'students'}
-    ];
-
   }]);
 })();
